a1b19e9f26ac403348fede95c335fb3f943d01bd
Nguyễn Tuấn Anh <anhnt683@gmail.com>
2024-01-07 12:51:02 +0700

    Top 5

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,13 +10,16 @@ class DashboardController extends Controller
 {
     public function index()
     {
+        // Fetch asset collections
         $response = Http::withHeaders([
             'Accept' => 'application/json',
             'x-api-key' => env('API_KEY'),
-        ])->get('https://api.gameshift.dev/asset-collections', [
-        ]);
+        ])->get('https://api.gameshift.dev/asset-collections');
+
         $jsonData = $response->json()['meta'];
-        $dataCount= $jsonData['totalResults'];
+        $dataCount = $jsonData['totalResults'];
+
+        // Fetch users
         $responseUser = Http::withHeaders([
             'Accept' => 'application/json',
             'x-api-key' => env('API_KEY'),
@@ -24,8 +27,44 @@ class DashboardController extends Controller
             'page' => 1,
             'perPage' => 999,
         ]);
+
+        $response1 = Http::withHeaders([
+            'accept' => 'application/json',
+            'content-type' => 'application/json',
+            'x-api-key' => env('API_KEY'),
+        ])->get('https://api.gameshift.dev/assets');
+
+        $res = $response1->json()['data'];
         $jsonDataUser = $responseUser->json();
         $dataCountUser = count($jsonDataUser['data']);
-        return view('admin.layouts.partials.main',compact('dataCount','dataCountUser'));
+
+        // Add points to users
+        $usersWithPoints = [];
+
+        foreach ($jsonDataUser['data'] as $user) {
+            $count_point = 0;
+
+            foreach ($res as $r) {
+                if ($r['name'] == $user['email']) {
+                    $count_point += (int) $r['attributes'][0]['value'];
+                }
+            }
+
+            $usersWithPoints[] = [
+                'user' => $user,
+                'count_point' => $count_point,
+            ];
+        }
+
+        // Sort users based on count_point in descending order
+        usort($usersWithPoints, function ($a, $b) {
+            return $b['count_point'] <=> $a['count_point'];
+        });
+
+        // Take the top 5 users
+        $top5Users = array_slice($usersWithPoints, 0, 5);
+
+        // Pass the data to the view
+        return view('admin.layouts.partials.main', compact('dataCount', 'dataCountUser', 'res', 'top5Users'));
     }
 }
